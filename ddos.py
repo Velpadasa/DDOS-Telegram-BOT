ac87c8e1043dda80023970cd6583385a4e9001c9
Velpadasa <velpadatanish@gmail.com>
2024-08-17 15:36:10 +0530

    Update ddos.py
    git clone https://github.com/akashblackhat/DDos_Attack.py.git
       ls
       cd DDos_Attack.py
       ls
       python NetSTRIK.py

--- ddos.py
+++ ddos.py
@@ -4,12 +4,12 @@ import socket
 import sys
 
 # Telegram API token
-TOKEN = "YOUR TOKEN HERE"
-
-
+TOKEN = 7108624217:AAHnpBNtXzbkSoL3G6eMemCCj7Kz75oZUYE
+bot /starrt
+bot your plan
 # Admin chat ID
-ADMIN_CHAT_ID = YOUR_ID_HERE
-
+ADMIN_CHAT_ID =7108624217
+bgmi servar attack 
 # Initialize bot
 bot = telebot.TeleBot(TOKEN)
 
@@ -50,7 +50,153 @@ def receive_target(message):
     target = message.text
     bot.send_message(chat_id=message.chat.id, text="Target received. Initiating Ping of Death attack.")
     # Start Ping of Death attack
-    perform_ping_of_death_attack(target)
+    perform_ping_of_death_attacktarget
+bot/ polling 
+Skip to content
+Navigation Menu
 
-# Start the bot
-bot.polling()
+code Search Results · org:blueskychan-dev DDOS attack
+14 files
+in
+blueskychan-dev
+(press backspace or delete to remove)
+
+
+blueskychan-dev/DDoSPacket · README.md
+…ues tab, Most issues i can resolved it, it look like my attack code/script is too old also i use Xamarin to based C# i…
+# DDoSPacket
+Use own Phone to DDoS Attack without Termux! (Android 5.1+)
+# What is DDoS Packet for Android?
+DDoS Packet is Dev project you can use phone to ddos attack without Termux or other program!
+* Q: This app can replace attacking by termux?
+[Click here!](https://github.com/fusedevgithub/DDoSPacket/issues)
+
+
+blueskychan-dev/dstatlayer7.github.io · index.html
+        <h3>DSTATLAYER7.ml is best for STRESS or attacking free web hosting LAYER 7!</h3>
+        <p>*We not recommends using ddos attacking for attack server without access but recommends for testing own serv…
+
+
+blueskychan-dev/android_kernel_samsung_msm8917_64-1 · Documentation/networking/ip-sysctl.txt
+	    the packet check will fail.
+	Current recommended practice in RFC3704 is to enable strict mode
+	to prevent IP spoofing from DDos attacks. If using asymmetric routing
+	or other complicated routing, then loose mode is recommended.
+	The max value from conf/{all,interface}/rp_filter is used
+Show 4 more matches
+
+
+blueskychan-dev/dstatlayer7.github.io · aboutus.html
+…hosting and select best the free hosting on soon anyways ddos attack without permission that bad for everyone and ille…
+
+
+blueskychan-dev/DDoSPacket-For-Windows · README.md
+# DDoSPacket-For-Windows
+DDoSPacket For Windows (Recommend Windows 8+)
+![DDoSPacketWindowsIcon](https://user-images.githubusercontent.com/47820634/161378851-3a6d57a5-b69d-4896-b8c8-67ea8b542…
+# What is DDoS Packet
+* DDoS Packet is ddos program write with C# and .NET for Troll someone or education
+* For tcp procotol if you starting attack and then that crash make sure target port are opened (we need connecting to s…
+* for udp and other sometimes attacking is working but target not got any packet (mean ISP are blocked or good firewall)
+DDoS Packet is have 2 platform support (Windows and Android)
+Show 2 more matches
+
+
+blueskychan-dev/DDoSPacket · DDoSPacket/DDoSPacket/DDoSPacket/MainPage.xaml.cs
+using Android.App;
+using System.Net.NetworkInformation;
+namespace DDoSPacket
+{
+    public partial class MainPage : ContentPage
+    {
+Show 20 more matches
+
+
+blueskychan-dev/DDoSPacket-For-Windows · DDoSPacket-For-Windows/DDoSPacket-For-Windows/Form1.cs
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+        public void udpattack()
+        {
+            
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+Show 14 more matches
+
+
+blueskychan-dev/DDoSPacket · DDoSPacketAndroid/DDoSPacketAndroid/DDoSPacketAndroid/MainPage.xaml.cs
+            }
+            return sb.ToString();
+        }
+        public void udpattack()
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+Show 20 more matches
+
+
+blueskychan-dev/FuseDDoS-Version3 · obj/Debug/DDoSAttack For Windows NET 4.6.1.csproj.FileListAbsolute.txt
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\bin\Debug\DDoSAttack For …
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\bin\Debug\DDoSAttack For …
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\bin\Debug\DDoSAttack For …
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\obj\Debug\DDoSAttack_For_…
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\obj\Debug\DDoSAttack_For_…
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\obj\Debug\DDoSAttack For …
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\obj\Debug\DDoSAttack For …
+c:\users\fusen\source\repos\DDoSAttack For Windows NET 4.6.1\DDoSAttack For Windows NET 4.6.1\obj\Debug\DDoSAttack For …
+Show 6 more matches
+
+
+blueskychan-dev/FuseDDoS-Version3 · DDoSAttack For Windows NET 4.6.1.csproj
+    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
+    <ProjectGuid>{07F52737-06D9-4E45-982F-03773FC6F543}</ProjectGuid>
+    <OutputType>WinExe</OutputType>
+    <RootNamespace>DDoSAttack_For_Windows_NET_4._6._1</RootNamespace>
+    <AssemblyName>DDoSAttack For Windows NET 4.6.1</AssemblyName>
+    <TargetFrameworkVersion>v4.6.1</TargetFrameworkVersion>
+    <FileAlignment>512</FileAlignment>
+
+
+blueskychan-dev/FuseDDoS-Version3 · obj/Debug/DDoSAttack For Windows NET 4.6.1.csproj.CoreCompileInputs.cache
+0699f9b68cd39ba688889ab5f18f0cceb586c8ee
+
+
+blueskychan-dev/FuseDDoS-Version3 · bin/Debug/DDoSAttack For Windows NET 4.6.1.exe.config
+<?xml version="1.0" encoding="utf-8" ?>
+<configuration>
+    <startup> 
+        <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.6.1" />
+    </startup>
+
+
+blueskychan-dev/FuseDDoS-Version3 · README.md
+Hi everyone FuseDDoS project status is cancel and out of dated but we have alternative project are better than this pro…
+* Link: [DDoSPacket for Windows](https://github.com/fusedevgithub/DDoSPacket-For-Windows/)
+* Feature: Change type of attack data, mutilthread, Many Platform Support, ICMP Support, Customable Attack size you can…
+# FuseDDoS-Version-3
+FuseDDoS Version 3 with C#! and Best of Testing Flood Layer 4!
+![Over1](https://raw.githubusercontent.com/PC1266/FuseDDoS-Version3/main/rererererere.PNG)
+FuseDDoS Version 2 and 1 Created for Edu and testing server
+FuseDDoS Version 3 Creating from C# are good rate code and going to support with other OS!
+Show 17 more matches
+
+
+blueskychan-dev/FuseDDoS-Version3 · Form1.Designer.cs
+﻿
+using System;
+namespace FuseDDoS_Version_3
+{
+    partial class Form1
+    {
+Show 5 more matches
+ We've excluded 5 identical files found across repositories.  
+ updater.dispatcher.add_handler(CommandHandler('t', tmps))
+
+updater.dispatcher.add_handler(CommandHandler('ddos', ddos))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('bot', google_bot))
+updater.dispatcher.add_handler(CommandHandler('stress', STRESS))
+updater.dispatcher.add_handler(CommandHandler('cfb', CFB))
+updater.dispatcher.add_handler(CommandHandler('cfbuam', CFBUAM))
+updater.dispatcher.add_handler(CommandHandler('tor', TOR))
+updater.dispatcher.add_handler(CommandHandler('ovh', OVH))
+#Run the bot
+updater.start_polling()
